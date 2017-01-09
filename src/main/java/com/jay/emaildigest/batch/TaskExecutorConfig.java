a5a3fde855a309171ea852df06fc046080b6c6a7
Jayasagar Jagirapu <jayasagar@gmail.com>
2017-01-09 18:20:11 +0100

    unneccessary imports

--- src/main/java/com/jay/emaildigest/batch/TaskExecutorConfig.java
+++ src/main/java/com/jay/emaildigest/batch/TaskExecutorConfig.java
@@ -1,13 +1,9 @@
 package com.jay.emaildigest.batch;
 
-import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.scheduling.annotation.AsyncConfigurer;
-import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
-import java.lang.reflect.Method;
 import java.util.concurrent.Executor;
 
 @Configuration
